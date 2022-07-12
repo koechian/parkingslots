ed6ed412457deb029cd0d170d11d825bbe34d036
koechian <ianovic8@live.com>
2022-07-12 18:47:11 +0300

    Slot occupancy tracking using amount of color

--- main.py
+++ main.py
@@ -1,37 +1,67 @@
+import numpy as np
 import pickle
-from pathlib import Path
 import cv2
-import pickle
+import cvzone
 
-WIDTH, HEIGHT = 107, 48
+from pathlib import Path
+from slots import *
 
-try:
-    with open("slots.p", "rb") as f:
-        nodePos = pickle.load(f)
-except:
-    nodePos = []
+with open("slots.p", "rb") as f:
+    nodePos = pickle.load(f)
+
+VIDEO_LOCATION = str(Path("data/overhead_parking.mp4"))
 
 
-def mouseCLick(event, x, y, flags, params):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        nodePos.append((x, y))
+def process(feed):
 
-    if event == cv2.EVENT_RBUTTONDOWN:
-        for i, pos in enumerate(nodePos):
-            x1, y1 = pos
-            if x1 < x < (x1 + WIDTH) and y1 < y < (y1 + HEIGHT):
-                nodePos.pop(i)
+    # the image has been converted to grayscale and blurred
+    processed = cv2.GaussianBlur((cv2.cvtColor(feed, cv2.COLOR_BGR2GRAY)), (3, 3), 1)
 
-    with open("slots.p", "wb") as f:
-        pickle.dump(nodePos, f)
+    # converting to binary image map
+    threshold = cv2.adaptiveThreshold(
+        processed, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16
+    )
 
+    # applying median blur to reduce noise
+    median = cv2.medianBlur(threshold, 5)
+
+    # stretching the pixels in the image to make it easier to find bounds
+
+    kernels = np.ones((3, 3), np.uint8)
+    dilate = cv2.dilate(median, kernels, iterations=1)
+    return dilate
+
+
+def checkSlot(processed):
+    for nd in nodePos:
+        x, y = nd
+        slot = processed[y : y + HEIGHT, x : x + WIDTH]
+
+        cv2.imshow(str(x * y), slot)
+
+        # counting all non-zero pixels
+        count = cv2.countNonZero(slot)
+        cvzone.putTextRect(
+            feed, str(count), (x, y + HEIGHT - 10), scale=1, thickness=2, offset=0
+        )
+
+
+cap = cv2.VideoCapture(VIDEO_LOCATION)
 
 while True:
-    img = cv2.imread(str(Path("data/overhead_parking.png")))
+    # looping the video
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    _, feed = cap.read()
+
+    # image processing
+    checkSlot(process(feed))
 
     for x in nodePos:
-        cv2.rectangle(img, x, (x[0] + WIDTH, x[1] + HEIGHT), [0, 255, 0], 2)
+        cv2.rectangle(feed, x, (x[0] + WIDTH, x[1] + HEIGHT), [0, 255, 0], 2)
 
-    cv2.imshow("out", img)
-    cv2.setMouseCallback("out", mouseCLick)
-    cv2.waitKey(1)
+    cv2.imshow("Video Feed", feed)
+    # cv2.imshow("Processed", process(feed))
+
+    cv2.waitKey(10)
---
+++ slots.py
@@ -0,0 +1,38 @@
+import pickle
+from pathlib import Path
+import cv2
+import pickle
+
+WIDTH, HEIGHT = 107, 48
+
+try:
+    with open("slots.p", "rb") as f:
+        nodePos = pickle.load(f)
+except:
+    nodePos = []
+
+
+def mouseCLick(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        nodePos.append((x, y))
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(nodePos):
+            x1, y1 = pos
+            if x1 < x < (x1 + WIDTH) and y1 < y < (y1 + HEIGHT):
+                nodePos.pop(i)
+
+    with open("slots.p", "wb") as f:
+        pickle.dump(nodePos, f)
+
+
+if __name__ == "__main__":
+    while True:
+        img = cv2.imread(str(Path("data/overhead_parking.png")))
+
+        for x in nodePos:
+            cv2.rectangle(img, x, (x[0] + WIDTH, x[1] + HEIGHT), [0, 255, 0], 2)
+
+        cv2.imshow("out", img)
+        cv2.setMouseCallback("out", mouseCLick)
+        cv2.waitKey(1)
